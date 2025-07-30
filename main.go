8ff23dee93b18640b246dcdbb11be7e510b3caf8
BeriutaH <13249837053@163.com>
2025-07-30 09:19:07 +0800

    数据库SQL工单平台

---
+++ main.go
@@ -0,0 +1,7 @@
+package main
+
+import "Yearn-go/src/lib/factory"
+
+func main() {
+	factory.Hello()
+}
---
+++ src/lib/factory/factory.go
@@ -0,0 +1,7 @@
+package factory
+
+import "fmt"
+
+func Hello() {
+	fmt.Println("Hello from factory!")
+}
